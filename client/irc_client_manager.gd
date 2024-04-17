349db29eda8fc4161e3e412e0e359c9b3d564ac3
Eggbertx <eggbertx@users.noreply.github.com>
2024-04-17 13:47:56 -0700

    Remove test connection

--- client/irc_client_manager.gd
+++ client/irc_client_manager.gd
@@ -11,34 +11,14 @@ var profile := IRCProfile.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	profile.nick = "GodotIRC"
-	profile.username = "godotirc"
-	profile.real_name = "Godot IRC"
-
-	var client_opts = IRCOptions.new()
-	client_opts.host = "irc.rizon.net"
-	client_opts.server_password = "test"
-	client_opts.channels.append("#codefest")
-
-	var client := IRCClient.new(client_opts, profile)
-	client.raw_message_received.connect(_on_raw_message_received)
-	client.server_message_received.connect(_on_server_message_received)
-	client.unhandled_message_received.connect(_on_unhandled_message)
-	var err := await client.start_connection()
-	assert(err == "")
-	add_child(client)
-	client_connected.emit(client)
-	client.name = client.host
-
+	pass
 
 func disconnect_clients():
 	var children := get_children()
 	for child in children:
 		var client = child as IRCClient
 		client.end_connection()
-		client.raw_message_received.disconnect(_on_raw_message_received)
-		client.server_message_received.disconnect(_on_server_message_received)
-		client.unhandled_message_received.disconnect(_on_unhandled_message)
+		_disconnect_client_signals(client)
 
 func get_server_conn(server: String) -> IRCClient:
 	var children := get_children()
@@ -47,6 +27,17 @@ func get_server_conn(server: String) -> IRCClient:
 		if client.host == server:
 			return client
 	return null
+
+func add_server(opts:IRCOptions, profile: IRCProfile) -> String:
+	var client := IRCClient.new(opts, profile)
+	_connect_client_signals(client)
+	var err := await client.start_connection()
+	if err != "":
+		return err
+	add_child(client)
+	client_connected.emit(client)
+	client.name = client.host
+	return ""
 
 func join_channel(server: String, channel: String):
 	var client = get_server_conn(server)
@@ -57,6 +48,15 @@ func join_channel(server: String, channel: String):
 	await client.join_channel(channel)
 	channel_joined.emit(server, channel)
 
+func _connect_client_signals(client:IRCClient):
+	client.raw_message_received.connect(_on_raw_message_received)
+	client.server_message_received.connect(_on_server_message_received)
+	client.unhandled_message_received.connect(_on_unhandled_message)
+
+func _disconnect_client_signals(client:IRCClient):
+	client.raw_message_received.disconnect(_on_raw_message_received)
+	client.server_message_received.disconnect(_on_server_message_received)
+	client.unhandled_message_received.disconnect(_on_unhandled_message)
 
 func _notification(what:int):
 	match what:
